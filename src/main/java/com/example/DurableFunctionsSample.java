1615bb462ff6ab0f4d953f000294d090e7518f1f
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-10 21:00:48 +0900

    implement fanout/fanin sample

--- src/main/java/com/example/DurableFunctionsSample.java
+++ src/main/java/com/example/DurableFunctionsSample.java
@@ -8,12 +8,18 @@ import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import com.microsoft.durabletask.DurableTaskClient;
 import com.microsoft.durabletask.OrchestrationRunner;
+import com.microsoft.durabletask.Task;
 import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
 import com.microsoft.durabletask.azurefunctions.DurableClientContext;
 import com.microsoft.durabletask.azurefunctions.DurableClientInput;
 import com.microsoft.durabletask.azurefunctions.DurableOrchestrationTrigger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DurableFunctionsSample {
     // クライアント関数。オーケストレーター関数を開始する
@@ -24,26 +30,55 @@ public class DurableFunctionsSample {
             final ExecutionContext context
             ) {
         DurableTaskClient client = durableContext.getClient();
-        String instanceId = client.scheduleNewOrchestrationInstance("HelloCities");
+//        String instanceId = client.scheduleNewOrchestrationInstance("HelloCities");
+        String instanceId = client.scheduleNewOrchestrationInstance("FanOutFanIn");
         context.getLogger().info("Created new Java orchestration with instance ID = " + instanceId);
         return durableContext.createCheckStatusResponse(req, instanceId);
     }
 
     // オーケストレーター関数。アクションが実行される方法とアクセスの実行順序を書く
-    @FunctionName("HelloCities")
-    public String helloCitiesOrchestrator(@DurableOrchestrationTrigger(name = "runtimeState") String runtimeState) {
+//    @FunctionName("HelloCities")
+//    public String helloCitiesOrchestrator(@DurableOrchestrationTrigger(name = "runtimeState") String runtimeState) {
+//        return OrchestrationRunner.loadAndRun(runtimeState, ctx -> {
+//            String result = "";
+//            result += ctx.callActivity("SayHello", "Tokyo", String.class).await() + ", ";
+//            result += ctx.callActivity("SayHello", "London", String.class).await() + ", ";
+//            result += ctx.callActivity("SayHello", "Seattle", String.class).await();
+//            return result;
+//        });
+//    }
+
+    @FunctionName("FanOutFanIn")
+    public String fanOutFanInOrchestrator(
+            @DurableOrchestrationTrigger(name = "runtimeState") String runtimeState
+    ) {
         return OrchestrationRunner.loadAndRun(runtimeState, ctx -> {
-            String result = "";
-            result += ctx.callActivity("SayHello", "Tokyo", String.class).await() + ", ";
-            result += ctx.callActivity("SayHello", "London", String.class).await() + ", ";
-            result += ctx.callActivity("SayHello", "Seattle", String.class).await();
-            return result;
+
+            List<?> batch = ctx.callActivity("F1", List.class).await();
+
+            List<Task<Integer>> parallelTasks = batch.stream()
+                    .map(i -> ctx.callActivity("F2", i, Integer.class))
+                    .collect(Collectors.toList());
+
+            List<Integer> results = ctx.allOf(parallelTasks).await();
+            return results.stream().reduce(0, Integer::sum);
         });
     }
 
-    // アクティブティ関数。作業を実行し、必要に応じて値を返す。
-    @FunctionName("SayHello")
-    public String sayHello(@DurableActivityTrigger(name = "name") String name) {
-        return String.format("Hello %s!", name);
+    @FunctionName("F1")
+    public List<Integer> f1(@DurableActivityTrigger(name = "f1name") String name) {
+        return new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
     }
+
+    @FunctionName("F2")
+    public Integer f2(@DurableActivityTrigger(name = "f2name") int i, final ExecutionContext context) {
+        context.getLogger().info("F2 with i = [" + i + "]");
+        return i * 3;
+    }
+
+    // アクティブティ関数。作業を実行し、必要に応じて値を返す。
+//    @FunctionName("SayHello")
+//    public String sayHello(@DurableActivityTrigger(name = "name") String name) {
+//        return String.format("Hello %s!", name);
+//    }
 }
