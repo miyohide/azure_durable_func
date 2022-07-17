3f58168218361cb9706c7f1a7922bce2aa3eb3d9
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-17 16:30:03 +0900

    refactoring

--- src/main/java/com/example/JobInfo.java
+++ src/main/java/com/example/JobInfo.java
@@ -1,15 +1,12 @@
 package com.example;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.microsoft.durabletask.DataConverter;
-
-import java.lang.reflect.InvocationTargetException;
 import java.time.Duration;
 import java.time.Instant;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-public class JobInfo implements DataConverter {
-    private String jobId;
+public class JobInfo {
+    private String jobId = "1";
 
     public String getJobId() {
         return jobId;
@@ -25,29 +22,5 @@ public class JobInfo implements DataConverter {
 
     public void setJobId(String jobId) {
         this.jobId = jobId;
-    }
-
-    @javax.annotation.Nullable
-    @Override
-    public String serialize(@javax.annotation.Nullable Object value) {
-        return null;
-    }
-
-    @javax.annotation.Nullable
-    @Override
-    public <T> T deserialize(@javax.annotation.Nullable String data, Class<T> target) {
-        T obj = null;
-        try {
-            obj = target.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-        return obj;
     }
 }
