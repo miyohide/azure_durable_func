03af95ee97ae699281edb23c2e05babf5ba30272
Hidenori Miyoshi <miyohide@gmail.com>
2022-07-17 15:35:49 +0900

    add Jackson config.

--- src/main/java/com/example/JobInfo.java
+++ src/main/java/com/example/JobInfo.java
@@ -1,11 +1,18 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.microsoft.durabletask.DataConverter;
+
+import java.lang.reflect.InvocationTargetException;
 import java.time.Duration;
 import java.time.Instant;
 
-public class JobInfo {
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class JobInfo implements DataConverter {
+    private String jobId;
+
     public String getJobId() {
-        return "1";
+        return jobId;
     }
 
     public Instant getExpirationTime() {
@@ -13,6 +20,34 @@ public class JobInfo {
     }
 
     public Duration getPollingDelay() {
-        return Duration.ofSeconds(1L);
+        return Duration.ofSeconds(10L);
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    @javax.annotation.Nullable
+    @Override
+    public String serialize(@javax.annotation.Nullable Object value) {
+        return null;
+    }
+
+    @javax.annotation.Nullable
+    @Override
+    public <T> T deserialize(@javax.annotation.Nullable String data, Class<T> target) {
+        T obj = null;
+        try {
+            obj = target.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+        return obj;
     }
 }
